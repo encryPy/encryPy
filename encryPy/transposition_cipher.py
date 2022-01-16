1000da31c0c9e1502ca12714286816ad5f79f095
ProsperChuks <prosperc40@gmail.com>
2022-01-16 10:05:32 +0100

    update: update decryption function

--- encryPy/transposition_cipher.py
+++ encryPy/transposition_cipher.py
@@ -1,14 +1,14 @@
 # Author: Prosper Chuks
 # License: MIT
 
-import string
 from typing import List
+import math
 
 
 def split_len(seq, length) -> List:
     return [seq[i:i + length] for i in range(0, len(seq), length)]
 
-def encode(self, key, plaintext) -> string:
+def encode(self, key, message)->str:
     
     self.order = {
         int(val): num for num, val in enumerate(key)
@@ -16,14 +16,14 @@ def encode(self, key, plaintext) -> string:
     self.ciphertext = ''
 
     for index in sorted(self.order.keys()):
-        for part in split_len(plaintext, len(key)):
+        for part in split_len(message, len(key)):
             try:
                 self.ciphertext += part[self.order[index]]
             except IndexError:
                 continue
     return self.ciphertext
 
-def encrypt(self, key, message) -> string:
+def encrypt(self, key, message)->str:
 
     self.ciphertext = [''] * key
     for col in range(key):
@@ -33,5 +33,16 @@ def encrypt(self, key, message) -> string:
             self.position += key
     return ''.join(self.ciphertext)
 
-def decrypt(self, message):
-    pass
+def decrypt(self, key, message)->str:
+
+    numOfColumns = math.ceil(len(message) / key)
+    numOfShadedBoxes = (numOfColumns * key) - len(message)
+    decrypted_message = float() * numOfColumns
+    col=0;row=0
+    for symbol in message:
+        decrypted_message[col] += symbol
+        col += 1
+        if col == numOfColumns or col == numOfColumns-1 & row >= key - numOfShadedBoxes:
+            col = 0
+            row += 1
+    return ''.join(decrypted_message)
