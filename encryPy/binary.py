ef3846c0c75a4684f06ce936208321bace239835
Prosper Chuks <prosperc40@gmail.com>
2022-01-17 15:47:57 -0800

    removed unused block of code

--- encryPy/binary.py
+++ encryPy/binary.py
@@ -34,7 +34,7 @@ class base:
                         self.encoded_arr.append(pd.DataFrame(store).T)
                         self.encoded_lst = []
             encoded_arr_np = np.concatenate(self.encoded_arr)
-            return pd.DataFrame(encoded_arr_np)
+            return pd.DataFrame(encoded_arr_np, columns=self.bin.columns)
             
         elif not dataframe.empty :
             
@@ -53,22 +53,5 @@ class base:
                         self.encoded_arr.append(pd.DataFrame(store).T)
                         self.encoded_lst = []
             encoded_arr_np = np.concatenate(self.encoded_arr)
-            return pd.DataFrame(encoded_arr_np)
-            
-        self.bin = pd.read_csv(path)
-        for n_row in range(len(self.bin)):
-            row = self.bin.loc[n_row]
-
-            for k, v in enumerate(row.T):
-                code = base64.b64encode(bytes(str(v).encode('ascii')))
-                self.encode_lst.append(code)
-
-                if len(self.encode_lst) == len(self.bin.columns):
-                    store = {
-                        (int(n_row) for n_row in range(k)): np.array([a for a in self.encode_lst])
-                        }
-                    self.encoded_arr.append(pd.DataFrame(store).T)
-                    self.encode_lst = []
-        encoded_arr_np = np.concatenate(self.encoded_arr)
-
-        return pd.DataFrame(encoded_arr_np, columns=self.bin.columns)
+            return pd.DataFrame(encoded_arr_np, columns=self.bin.columns)
+        
