47108419d27553b4d6d62141c0faa416224ebab9
ProsperChuks <prosperc40@gmail.com>
2022-01-18 13:23:47 +0100

    feat: add XOR data encryption

---
+++ encryPy/or/xor.py
@@ -0,0 +1,18 @@
+# Author: Prosper Chuks
+# License: MIT
+
+import pandas as pd
+import numpy as np
+
+
+def xor():
+    pass
+
+def _xor_process():
+    pass
+
+def encrypt_data():
+    pass
+
+def decrypt_data():
+    pass
