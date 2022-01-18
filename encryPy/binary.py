7ec98c4d8ec719641712ab4c6ec2bbe1ef849208
ProsperChuks <prosperc40@gmail.com>
2022-01-18 01:52:08 +0100

    update: add binary data decoder

--- encryPy/binary.py
+++ encryPy/binary.py
@@ -12,6 +12,8 @@ class base:
         try:
             self.encoded_lst = []
             self.encoded_arr = []
+            self.decoded_lst = []
+            self.demb = []
         except: 
             raise NotImplementedError
 
@@ -54,4 +56,24 @@ class base:
                         self.encoded_lst = []
             encoded_arr_np = np.concatenate(self.encoded_arr)
             return pd.DataFrame(encoded_arr_np, columns=self.bin.columns)
+    
+    def decode(self, dataframe: pd.DataFrame) -> pd.DataFrame:
+
+        self.bin =  pd.DataFrame(dataframe)
+        for n_row in range(len(self.bin)):
+            row = self.bin.loc[n_row]
+
+            for k, v in enumerate(row.T):
+                code = base64.b64decode(v)
+                self.decoded_lst.append(code)
+
+                if len(self.decoded_lst) == len(self.bin.columns):
+                    store = {
+                        (int(n_row) for n_row in range(k)): np.array([a for a in self.decoded_lst])
+                        }
+                    self.decoded_arr.append(pd.DataFrame(store).T)
+                    self.decoded_lst = []
         
+        decoded_arr_np = np.concatenate(self.decoded_arr)
+
+        return pd.DataFrame(decoded_arr_np.astype(str), columns=self.bin.columns)
