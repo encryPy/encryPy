7468e169333e70d216004363453fe5e331d40738
ProsperChuks <prosperc40@gmail.com>
2022-01-18 15:30:36 +0100

    xor encrypt processor

--- encryPy/or/xor.py
+++ encryPy/or/xor.py
@@ -8,8 +8,13 @@ import numpy as np
 def xor():
     pass
 
-def _xor_process():
-    pass
+def _xor_process(data, key):
+    ''' '''
+    data_len = len(data)
+    for c in range(data_len):
+        data = (data[:c] +chr(ord(data[c]) ^ ord(key)) + data[c+1:])
+    
+    return data
 
 def encrypt_data():
     pass
